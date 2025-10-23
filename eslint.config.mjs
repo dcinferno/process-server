9b4dcde4462518e659023e55aed283e6620e1847
derek-charles <52976014+derek-charles@users.noreply.github.com>
2025-10-23 07:08:48 -0500

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
+
   {
     ignores: [
       "node_modules/**",
@@ -19,6 +21,37 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+  },
+
+  {
+    plugins: {
+      import: importPlugin,
+    },
+
+    languageOptions: {
+      globals: {},
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+
+    rules: {
+      "import/no-relative-parent-imports": "warn",
+    },
+
+    settings: {
+      "import/resolver": {
+        node: {
+          paths: ["src"],
+          extensions: [".js", ".jsx"],
+        },
+        alias: {
+          map: [["@", "./src"]],
+          extensions: [".js", ".jsx"],
+        },
+      },
+    },
   },
 ];
 
