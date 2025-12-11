da976f9b2e07e656fc88684a4c85425d272627c6
derek-charles <52976014+derek-charles@users.noreply.github.com>
2025-12-10 21:04:24 -0600

    OPTIONS

--- src/app/api/stripe/webhook/route.js
+++ src/app/api/stripe/webhook/route.js
@@ -8,6 +8,9 @@ export const dynamic = "force-dynamic";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
+export async function OPTIONS() {
+  return new Response("OK", { status: 200 });
+}
 /**
  * Send sale notification to Telegram group
  */
